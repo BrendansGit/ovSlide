6a9145bc3220feb02e095bb64bdedb83a54b9e94
Brendan Mullins <b.mullins@agentur-loop.com>
2016-06-20 18:33:43 +0200

    run setup on resize

--- ovSlide.js
+++ ovSlide.js
@@ -1,108 +1,195 @@
-function scrollControl(main, dialog, preload){
+var ovslider = function(el, minwidth, gutter){
 	var self = this;
-	self.$main = $(main);
-	self.$dialog = $(dialog);
-	self.$preload = $(preload);
+	
+	el = $(el);
+	var wrapper = el.children().first();
+	this.els = wrapper.children();
 
-	if (!self.$main.length) {console.error('ScrollControl: no main found', main) }
-	if (!self.$dialog.length) {console.error('ScrollControl: no dialog found', dialog) }
-	if (!self.$preload.length) {console.error('ScrollControl: no preload found', preload) }
 
-	self.originalMarginTop = parseInt(self.$main.css('margin-top'));
-	self.saveScrollPos = 0;
-	self.stateIs = '';
-	self.set = function(key) {
-		self.stateIs = (key);
-		var sct = $(window).scrollTop();
-		var h = $(window).height();
+	this.elsInView = this.els.length;
+	this.elsPassed = 0;
+	this.elsOver = 0;
+	this.absPos = 0;
+	this.panPos = 0;
 
-		if (key === 'dialog') {
-			// dialog
-			window.navigation.navLock = 1;
-			$(window).scrollTop(0);
-			self.saveScrollPos = sct;
-			self.$main.css({
-				position: 'fixed',
-				'margin-top': sct*-1 + self.originalMarginTop,
-				// 'margin-right': self.scrollBarWidth,
-				overflow: 'hidden',
-				top: 0,
-				bottom: 0,
-				left: 0,
-				right: 0
-			}).addClass('disabled');
+	this.elWidth = minwidth;
 
-			self.$dialog.addClass('is-visible');
+	var c = {
+		out: 'slide-out',
+		in: 'slide-in',
+		left: 'slide-out-left',
+		right: 'slide-out-right'
+	};
+
+	this.setup = function(){
+		var overflow = 0;
+		self.elWidth = el.width() / self.els.length;
+		while(self.elWidth < minwidth) {
+			overflow++;
+			self.elsInView = self.els.length - overflow;
+			self.elWidth = el.width() / self.elsInView;
 		}
-		if (key === 'preload') {
-			// preload
-			window.navigation.navLock = 1;
-			$(window).scrollTop(0);
-			self.saveScrollPos = sct;
-			self.$main.css({
-				position: 'fixed',
-				'margin-top': sct*-1 + self.originalMarginTop,
-				// 'margin-right': self.scrollBarWidth,
-				overflow: 'hidden',
-				top: 0,
-				bottom: 0,
-				left: 0,
-				right: 0
-			}).addClass('disabled');
+		var totalWidth = self.elWidth * self.els.length;
+		totalWidth = totalWidth + (gutter * self.els.length);
 
-			// self.$dialog.removeClass('is-visible');
-		}
-		if (key === 'main') {
-			// main
-			window.navigation.navLock = 0;
-			self.$main.css({
-				position: 'static',
-				'margin-top': self.originalMarginTop,
-				// 'margin-right': 0,
-				overflow: 'visible',
-				top: 'auto',
-				bottom: 'auto'
-			}).removeClass('disabled');;
-			$(window).scrollTop(self.saveScrollPos + self.originalMarginTop);
+		wrapper.css('width', totalWidth);
+		self.els.css('width', self.elWidth);
 
-			self.$dialog.removeClass('is-visible');
+		setTimeout(function(){
+			window.menublocks.synch();
+			window.menuTitles.synch();
+		},50);
+	};
+	this.setupSlides = function(){
+		if (self.elsPassed < 0) {
+			self.elsPassed = 0;
 		}
+		self.elsOver = self.els.length - (self.elsInView + self.elsPassed);
+
+		var maxout = 50;
+		while (self.elsOver < 0) {
+			self.elsPassed--;
+			self.elsOver = self.els.length - (self.elsInView + self.elsPassed);
+			maxout--;
+			if (maxout === 0) {
+				console.error('not enough Elements');
+				return false;
+			}
+		}
+
+		self.els.each(function(i){
+			var $t = $(this);
+			var e = i+1;
+			if (e <= self.elsPassed) {
+				$t.removeClass([c.in, c.right].join(' ')).addClass([c.out, c.left].join(' '));
+			}
+			if (e > self.elsPassed && e <= (self.elsInView + self.elsPassed)) {
+				$t.removeClass([c.right, c.left, c.out].join(' ')).addClass([c.in].join(' '));
+			}
+			if (e > (self.elsInView + self.elsPassed)) {
+				$t.removeClass([c.in, c.left].join(' ')).addClass([c.out, c.right].join(' '));
+			}
+		});
+
+		if (self.elsPassed > 0) {
+			el.addClass('overflow-left');
+		}else{
+			el.removeClass('overflow-left');
+		}
+		if (self.elsOver > 0) {
+			el.addClass('overflow-right');
+		}else{
+			el.removeClass('overflow-right');
+		}
+
+		var negPos = self.elWidth * self.elsPassed;
+		wrapper.css('transform', 'translate3D(-'+negPos+'px, 0, 0)');
+		self.absPos = negPos*-1;
+	};
+
+	this.goToSlide = function(ind) {
+		if (ind === '+') {
+			self.elsPassed++;
+			self.setupSlides();
+		}else if (ind === '-') {
+			self.elsPassed--;
+		}else{
+			self.elsPassed = ind - 1;
+		}
+		self.setupSlides();
 	}
 
-	this.getScrollbarWidth = function() {
-	    var outer = document.createElement("div");
-	    outer.style.visibility = "hidden";
-	    outer.style.width = "100px";
-	    outer.style.msOverflowStyle = "scrollbar"; // needed for WinJS apps
+	this.runSetup = function(){
+		self.setup();
+		self.setupSlides();
+	};
+	this.runSetup();
 
-	    document.body.appendChild(outer);
+	this.pan = function(pos) {
+		var negPos = self.absPos + (pos);
+		wrapper.css({
+			transform: 'translate3D('+negPos+'px, 0, 0)'
+		});
+		self.panPos = pos;
+	};
 
-	    var widthNoScroll = outer.offsetWidth;
-	    // force scrollbars
-	    outer.style.overflow = "scroll";
+	wrapper.on('touchstart', function(){
+		wrapper.css({
+			transition: 'none',
+			'-webkit-transition': 'none'
+		});
+	}).on('touchend', function(){
+		// setTimeout(function(){
+			wrapper.css({
+				transition: 'transform 450ms',
+				'-webkit-transition': 'transform 450ms'
+			});
+		// });
+		var panDir = '+';
+		var panAmount = (self.panPos / self.elWidth);
+		var moveBy = 0;
 
-	    // add innerdiv
-	    var inner = document.createElement("div");
-	    inner.style.width = "100%";
-	    outer.appendChild(inner);        
+		// if (self.panPos < -80 || self.panPos > 80) {
+			if (panAmount < 0) {
+				panDir = '-';
+				panAmount= panAmount*-1;
+			}
 
-	    var widthWithScroll = inner.offsetWidth;
+			moveBy = Math.floor(panAmount + 0.6);
+		// }
 
-	    // remove divs
-	    outer.parentNode.removeChild(outer);
+		setTimeout(function(){
+			if (moveBy) {
+				// console.log(moveBy, panDir, self.elsPassed);
+				if (panDir == '+') {
+					self.elsPassed = self.elsPassed - moveBy;
+				}else{
+					self.elsPassed = self.elsPassed + moveBy;
+				}
 
-	    self.scrollBarWidth = widthNoScroll - widthWithScroll;
-	}	
-	this.getScrollbarWidth();
+				self.setupSlides();
+			}else{
+				wrapper.css({
+					transform: 'translate3D('+self.absPos+'px, 0, 0)'
+				});
+			}
+		});
+	});
 
-	var mainContent = this.$main.html().replace(/\n/g, '').length;
-	if (!mainContent) {
-		
-		this.$main.load(window.location.protocol + '//' + window.location.host + '/' + ' .main__content', function(){
-			$(window).trigger('scroll');
-			$(document).trigger('__ready');
-		})
-	}
+	this.hammer = new Hammer(el[0]);
+	this.hammer.on('pan', function(ev) {
+		if (ev.deltaY < 100 && ev.deltaY > -100) {
+			self.pan(ev.deltaX);
+		}
+	});
+
+	var to;
+	$(window).on('resize', function(){
+		clearTimeout(to);
+		to = setTimeout(function(){
+			self.setup();
+			self.runSetup();
+		}, 200);
+	});
 }
 
-var scroll = new scrollControl('.main', '.dialog', '.header');
+$(document).on('__ready', function(){
+	var menulist = new ovslider('.markthalle__menu', 210, 0);
+	// $(document).off('click').on('click', '.slide-out-left', function(){
+	// 	console.log('asd');
+	// 	menulist.goToSlide('-');
+	// });
+	// $(document).off('click').on('click', '.slide-out-right', function(){
+	// 	menulist.goToSlide('+');
+	// });
+	$('.markthalle__menu__card').on('click', function () {
+		var r = $(this).hasClass('slide-out-right');
+		var l = $(this).hasClass('slide-out-left');
+		if(r === true) {
+			menulist.goToSlide('+');
+		}
+		if(l === true) {
+			menulist.goToSlide('-');
+		}
+	});
+});
